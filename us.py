261524240bba75aad6619bec1f5614d61eab406c
Mosnio <130111318+Mosnio@users.noreply.github.com>
2024-01-31 12:52:47 +0530

    Update us.py

--- us.py
+++ us.py
@@ -131,8 +131,8 @@ cap = api.run(data)
 signup_payload = {
     'csrf_token': '',
     'op': 'signup_new',
-    'password': 'F4yheLLykj',
-    'email': 'anthony.ortiz@gmail.com','fingerprint':'1fad3fde0dc3279a86f55e445c824847',
+    'password': 'IR4GB7359k',
+    'email': 'abigail.diaz@gmail.com','fingerprint':'1fad3fde0dc3279a86f55e445c824847',
     'referrer':'17082',
     'h_recaptcha_response':cap
 }
